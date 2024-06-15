606d0889991ee8837bfba51f39a62047fc532353
Geevarghese George <4496097+thatgeeman@users.noreply.github.com>
2024-06-16 01:13:04 +0200

    complete db with postgres

--- README.md
+++ README.md
@@ -21,3 +21,30 @@ Testing the containers: In two containers for the consumer side, do:
 
 For the producer side: 
 `docker compose exec -it kafka  /opt/bitnami/kafka/bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --list`
+
+# postgres db 
+
+Download sample dataset [pagilla](https://github.com/devrimgunduz/pagila). You can load them via psql, pgAdmin, etc.
+```sh
+brew install libpq # for psql cli
+brew install postgresql@14 # for postgres14
+```
+To setup the user needed for postgres, follow the instructions here: https://gist.github.com/dnovais/c6c6894b95d764be2aca9736436edd0e
+
+Download [DBeaver](https://dbeaver.io/). 
+
+In the DBeaver, load the unzipped sql files and setup a conneciton to the database from the toolbar. Then set the schema from the schema.sql file.
+Now click on execute script. Similary open the pagilla-data.sql file and execute. Skip errors.
+
+^ this had to work, but for me what worked is:
+
+```sh
+/usr/local/Cellar/postgresql@14/14.12//bin/createuser -s postgres
+psql -f ../Desktop/pagila-0.10.1/pagila-data.sql -U postgres
+psql -f ../Desktop/pagila-0.10.1/pagila-schema.sql -U postgres
+```
+now we can go in the cli command line to do `psql -U postgres` and run `\d` to show the tables in this user.
+
+And run sql commands like `select actor_id, first_name from actor;`
+
+Following should work from docker: https://github.com/devrimgunduz/pagila?tab=readme-ov-file#create-database-on-docker
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import psycopg
 import requests
 
 url = "https://www.manning.com/"
@@ -12,3 +13,39 @@ with open("saved_page.html", "w", encoding="utf-8") as f:
     f.write(response.text)
 
 print(f"Successfully saved {url} as saved_page.html")
+
+# Replace placeholders with your actual connection details
+connection_parameters = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "",
+    "host": "localhost",  # defaults to localhost if not provided
+    "port": "5432",  # defaults to 5432 if not provided
+}
+connected = False
+try:
+    connection = psycopg.connect(**connection_parameters)
+    connected = True
+except psycopg.Error as error:
+    print("Error connecting to PostgreSQL database:", error)
+
+
+if connected:
+    print("Connected to PostgreSQL database!")
+    # Connect to an existing database
+    with connection as conn:
+        # Open a cursor to perform database operations
+        with conn.cursor() as cur:
+            """cur.execute(
+                    "INSERT INTO public.country (country_id, country) VALUES (%s, %s)",
+                    (
+                        999,
+                        "Moon Zone",
+                    ),
+            )"""
+            cur.execute(
+                "UPDATE public.country set country_id=1000 WHERE country='Moon Zone'"
+            )
+            print("Ran SQL command")
+            # Make the changes to the database persistent
+            conn.commit()
